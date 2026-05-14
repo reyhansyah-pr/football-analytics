f614e888a6ee3349429d3f104be83fa139de4b25
reyhansyah <reyhansyah23@gmail.com>
2026-05-14 19:02:07 +0700

    add scraping manager to main.py

--- etl/main.py
+++ etl/main.py
@@ -5,11 +5,21 @@ load_dotenv()
 
 from premier_league import PremierLeagueScraper
 from postgre_loader import PostgresLoader
+from scrape_wiki import WikiTableParser
 
 
 if __name__ == "__main__":
-
     print("Starting ETL Process...")
+    print("="*50)
+    print("Start Scraping List Manager Premier League from Wikipedia...")
+
+    parser = WikiTableParser('https://en.wikipedia.org/wiki/List_of_Premier_League_managers')
+    manager_df = parser.parse_managers_table()
+
+    print("Scraping Completed. Sample Data:")
+    print(manager_df.head(3))
+    print("="*50)
+    print("Fetching Premier League match data via REST API...")
 
     scraper = PremierLeagueScraper(season_start=2025, season_end=2025)
 
@@ -20,13 +30,19 @@ if __name__ == "__main__":
     loader = PostgresLoader(
         user=os.getenv("POSTGRES_USER"),
         password=os.getenv("POSTGRES_PASSWORD"),
-        host=os.getenv("POSTGRES_HOST", "localhost"),
+        host=os.getenv("POSTGRES_HOST"),
+        # host='localhost',
         port=5432,
         database=os.getenv("POSTGRES_DB")
     )
 
+    print("Starting Data Load into PostgreSQL...")
+
     loader.load_dataframe(teams_df, "raw_teams", schema="raw")
     loader.load_dataframe(players_df, "raw_players", schema="raw")
     loader.load_dataframe(matches_df, "raw_matches", schema="raw")
+    loader.load_dataframe(manager_df, "raw_managers", schema="raw")
+
+    print("Data Load Completed.")
 
     print("ETL Process Completed Successfully!")
